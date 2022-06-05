cfa5d757c1fc60de6ec30d1ff3af3a256f1fe9c7
Sousafer <fernando.ffsousa@gmail.com>
2022-06-05 20:18:23 +0000

    commit final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +6,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  generateRandomIcon1();
+};
+let AllIcons = document.querySelectorAll(".icon");
+
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let nNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[nNumbers];
+}
+let icon = ["♦", "♠", "♥", "♣"];
+const generateRandomIcon1 = () => {
+  let nIcon = icon[Math.floor(Math.random() * icon.length)];
+  for (let icons = 0; icons < AllIcons.length; icons++) {
+    AllIcons[icons].innerHTML = nIcon;
+    if (nIcon == "♦" || nIcon == "♥") {
+      AllIcons[icons].style.color = "red";
+    }
+  }
 };
